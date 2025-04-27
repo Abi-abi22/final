1fe4e2bcbefbae3e2a8b5e37b950cee8f12abe9d
Abi-abi22 <abinobi6@gmail.com>
2025-04-27 18:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def index():
     return render_template('index.html', recommendations=recommendations)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=100000)
